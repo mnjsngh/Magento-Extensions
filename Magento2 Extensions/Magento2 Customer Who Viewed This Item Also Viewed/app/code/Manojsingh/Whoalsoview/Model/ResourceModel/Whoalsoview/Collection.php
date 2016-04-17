775ec4af2fcce405f2e49527be32176e8f953d87
Manoj Singh <mnjsngh101@gmail.com>
2016-04-17 22:34:22 +0530

    updated

--- Magento2 Extensions/Magento2 Customer Who Viewed This Item Also Viewed/app/code/Manojsingh/Whoalsoview/Model/ResourceModel/Whoalsoview/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Manojsingh\Whoalsoview\Model\ResourceModel\Whoalsoview;
-class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
-{
-    protected function _construct()
-    {
-        $this->_init('Manojsingh\Whoalsoview\Model\Whoalsoview', 'Manojsingh\Whoalsoview\Model\ResourceModel\Whoalsoview');
-    }
-}
